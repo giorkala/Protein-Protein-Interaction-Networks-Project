109d23458ab38b8db11f4b06e0a02e37eec8969e
giorkala <giorkala@hotmail.com>
2018-10-31 02:52:42 +0000

    folder updating

--- PPIN_Construction/PPINanalyse.py
+++ PPIN_Construction/PPINanalyse.py
@@ -27,9 +27,9 @@ import pandas as pd
 # Work with csv module instead of pandas
 datafile = "EdgeLists/BIOGRID-ORGANISM-Homo_sapiens-3.5.165.edgelist"
 datafile = "EdgeLists/BIOGRID-ORGANISM-Human_Immunodeficiency_Virus_1-3.5.165.edgelist"
-
+datafile = "../../edge_list.txt"
 #print("translate edgelist")
-#Dictionary = ChangeNames( datafile )
+Dictionary = ChangeNames( datafile )
 
 edgelist = open(datafile, 'rb')
 G = nx.Graph()
