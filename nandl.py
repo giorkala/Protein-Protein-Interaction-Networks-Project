99368ac9f147ae4236a659bf1599763cc28e03cb
andrei-stoica26 <44497020+andrei-stoica26@users.noreply.github.com>
2018-11-08 12:09:10 +0000

    Update nandl.py

--- nandl.py
+++ nandl.py
@@ -1,14 +1,25 @@
 import os
 import linecache
-for filename in os.listdir('TFD2'):
-	nb = linecache.getline('TFD2/'+filename, 3)
-        nb2 = linecache.getline('TFD2/'+filename, 4)
-        if nb2[0]!='[':
-		nb+=nb2
+def writefile (folder):
+	newfolder=folder.replace('TFD','NL')
+	print(newfolder)
+	if not os.path.exists(newfolder):
+		os.makedirs(newfolder)
+	nb = linecache.getline(folder+filename, 3)
+	nb = linecache.getline(folder+filename, 3)
+        nextline = linecache.getline(folder+filename, 4)
+	i=4
+        while len(nextline)>0 and nextline[0]!='[':
+		nb+=nextline
+		i+=1
+		nextline = linecache.getline(folder+filename, i)
 	nb=nb.replace('[','').replace('.','').replace(']','').replace(',','').split()
-        g=open('NL/'+filename,'w')
+        g=open(newfolder+filename,'w')
 	ll=len(nb)+1
-	#print(nb)
         for i in range (1,ll):
         	g.write(str(i)+' '+nb[i-1]+'\n')
 	g.close()
+for filename in os.listdir('Distmat'):
+	writefile('TFD/')
+	writefile('TFD_LCC/')
+	
